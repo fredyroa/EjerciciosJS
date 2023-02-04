557441361da331fe0c1573873d992f7b1fbb67fd
fredyroa <68354410+fredyroa@users.noreply.github.com>
2023-02-04 17:27:54 -0500

    ejercicio10

---
+++ Ejercicio10/index.js
@@ -0,0 +1,9 @@
+function checkNumber(numero) {
+    if(numero % 2 == 0) {
+      return "Numero par";
+    }
+    else {
+      return "Numero impar";
+    }
+  }
+  console.log('Ejecute la funcion checkNumber() poniendo un numero entero entre los paréntesis');
